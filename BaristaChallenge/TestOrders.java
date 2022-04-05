8893efca901d64d26ed8384ca458cac41ab53407
LaurenOdalen <laurenodalen@gmail.com>
2022-04-05 12:12:25 -0600

    commit

--- BaristaChallenge/TestOrders.java
+++ BaristaChallenge/TestOrders.java
@@ -22,17 +22,11 @@ public class TestOrders {
         
         Order order5 = new Order("Jimmy");
 
-
-        order1.addItem(item1);
-        order1.addItem(item2);
-        order2.addItem(item3);
-        order2.addItem(item4);
-        order3.addItem(item2);
-        order3.addItem(item2);
-        order4.addItem(item3);
-        order4.addItem(item1);
-        order5.addItem(item4);
-        order5.addItem(item1);
+        order1.addItem(item1); order1.addItem(item2);
+        order2.addItem(item3); order2.addItem(item4);
+        order3.addItem(item2); order3.addItem(item2);
+        order4.addItem(item3); order4.addItem(item1);
+        order5.addItem(item4); order5.addItem(item1);
 
         System.out.println(order3.getStatusMessage());
 
